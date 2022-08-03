55ce8facb87050d70c717e513f4330c03b8c984e
Evgeny <evgenyturilov@gmail.com>
2022-08-03 14:24:15 +0300

    added extra code to Lecture_1

--- Lecture_1/Ex_002/Point2D.java
+++ Lecture_1/Ex_002/Point2D.java
@@ -1,8 +1,16 @@
 package Lecture_1.Ex_002;
 
-public class Point2D {
-    int x, y;
 
+/**
+ * Это точка 2D
+ */
+public class Point2D {
+    private int x, y;
+    /**
+     * Это конструктор ...
+     * @param valueX это координата x
+     * @param valueY это координата y
+     */
     public Point2D(int valueX, int valueY) {
         x = valueX;
         y = valueY;
@@ -16,6 +24,22 @@ public class Point2D {
 
     public Point2D() {
         this(0);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int value) {
+        this.x = value;
+    }
+
+    public void setY(int value) {
+        this.y = value;
     }
     // public Point2D() {
     //     x = 0;
@@ -34,6 +58,10 @@ public class Point2D {
     @Override
     public String toString() {
         return getInfo();
+    }
+
+    static double distance (Point2D a, Point2D b ) {
+        return Math.sqrt(Math.pow(a.getX() - b.getX(), 2) + Math.pow(a.getY() - b.getY(), 2));
     }
     // ctor
     // get; set;
--- Lecture_1/Ex_002/Programm.java
+++ Lecture_1/Ex_002/Programm.java
@@ -1,18 +1,19 @@
 package Lecture_1.Ex_002;
 
 public class Programm {
-    static double distance (Point2D a, Point2D b ) {
-        return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
-    }
+    
 
-    static double distance (int x1, int y1, int x2, int y2 ) {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-    }
+    // static double distance (int x1, int y1, int x2, int y2 ) {
+    //     return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    // }
     
     public static void main(String[] args) {
         Point2D a = new Point2D();
+        a.setX(20);
         // a.x = 0;
         // a.y = 2;
+        System.out.println(a.getX());
+        System.out.println(a.getY());
         System.out.println(a);
 
         Point2D b = new Point2D(10);
@@ -20,6 +21,9 @@ public class Programm {
         //b.y = 10;
         System.out.println(b);
 
-        System.out.println(distance(a, b));
+        //System.out.println(distance(a, b));
+
+        var dis = Point2D.distance(a, b);
+        System.out.println(dis);
     }
 }
